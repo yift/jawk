97e30f168cadec965a837ff03c835a0d96533db4
yift <me@ykaplan.me>
2023-12-10 20:01:38 +0000

    More unit tests

--- src/grouper.rs
+++ src/grouper.rs
@@ -70,3 +70,173 @@ impl Output for ActiveGrouper {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{mem, sync::Mutex};
+
+    use super::*;
+
+    #[test]
+    fn test_from_str_valid_name() -> Result<(), SelectionParseError> {
+        let str = ".a";
+
+        let grouper = Grouper::from_str(str)?;
+
+        let mut input = IndexMap::new();
+        input.insert("a".to_string(), JsonValue::String("yes".into()));
+        input.insert("b".to_string(), JsonValue::String("no".into()));
+        assert_eq!(
+            grouper.group_by.name(&input.into()),
+            Some("yes".to_string())
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_invalid_name() -> Result<(), SelectionParseError> {
+        let str = "(";
+
+        let result = Grouper::from_str(str);
+
+        assert!(matches!(result, Err(_)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_start_removes_headers() -> Result<(), SelectionParseError> {
+        let str = ".a";
+        let grouper = Grouper::from_str(str)?;
+        let called_by = Arc::new(Mutex::new(None));
+
+        struct MockOutput {
+            called_by: Arc<Mutex<Option<bool>>>,
+            me: bool,
+        }
+        impl Output for MockOutput {
+            fn output_row(&mut self, _: &JsonValue, _: Vec<Option<JsonValue>>) -> std::fmt::Result {
+                let mut called_by = self.called_by.lock().unwrap();
+                let _ = mem::replace(&mut *called_by, Some(self.me));
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                Some(Box::new(MockOutput {
+                    called_by: self.called_by.clone(),
+                    me: !self.me,
+                }))
+            }
+        }
+        let first_object = MockOutput {
+            called_by: called_by.clone(),
+            me: false,
+        };
+
+        let mut started = grouper.start(Arc::new(vec![]), Box::new(first_object));
+
+        started.output_row(&JsonValue::null(), vec![]).unwrap();
+        started.done().unwrap();
+
+        assert_eq!(*called_by.lock().unwrap(), Some(true));
+        Ok(())
+    }
+
+    #[test]
+    fn test_group_the_values_correctly() -> Result<(), SelectionParseError> {
+        let str = ".a";
+        let grouper = Grouper::from_str(str)?;
+        let got_values = Arc::new(Mutex::new(Vec::new()));
+
+        struct MockOutput {
+            got_values: Arc<Mutex<Vec<IndexMap<String, JsonValue>>>>,
+        }
+        impl Output for MockOutput {
+            fn output_row(
+                &mut self,
+                value: &JsonValue,
+                _: Vec<Option<JsonValue>>,
+            ) -> std::fmt::Result {
+                let mut got_values = self.got_values.lock().unwrap();
+                let map: IndexMap<String, JsonValue> = value.clone().try_into().unwrap();
+                got_values.push(map);
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                None
+            }
+        }
+        let first_object = MockOutput {
+            got_values: got_values.clone(),
+        };
+
+        let mut started = grouper.start(
+            Arc::new(vec!["title-1".to_string(), "title-2".to_string()]),
+            Box::new(first_object),
+        );
+
+        started
+            .output_row(&JsonValue::null(), vec![None, None])
+            .unwrap();
+        let mut value = IndexMap::new();
+        value.insert("a".to_string(), JsonValue::String("one".into()));
+        started
+            .output_row(&value.into(), vec![None, Some(100.into())])
+            .unwrap();
+        let mut value = IndexMap::new();
+        value.insert("a".to_string(), JsonValue::String("two".into()));
+        started
+            .output_row(&value.into(), vec![Some(10.into()), Some(150.into())])
+            .unwrap();
+        let mut value = IndexMap::new();
+        value.insert("a".to_string(), JsonValue::String("one".into()));
+        started
+            .output_row(&value.into(), vec![Some(40.into()), None])
+            .unwrap();
+        started.done().unwrap();
+
+        assert_eq!(got_values.lock().unwrap().len(), 1);
+        let value = got_values.lock().unwrap();
+        let value = value.first().unwrap();
+        assert_eq!(value.len(), 2);
+        let one: Vec<JsonValue> = value.get("one").unwrap().clone().try_into().unwrap();
+        let two: Vec<JsonValue> = value.get("two").unwrap().clone().try_into().unwrap();
+        assert_eq!(one.len(), 2);
+        assert_eq!(two.len(), 1);
+        let one_1: IndexMap<String, JsonValue> = one.get(0).unwrap().clone().try_into().unwrap();
+        assert_eq!(one_1.len(), 1);
+        assert_eq!(one_1.get("title-2").cloned(), Some(100.into()));
+        let one_2: IndexMap<String, JsonValue> = one.get(1).unwrap().clone().try_into().unwrap();
+        assert_eq!(one_2.len(), 1);
+        assert_eq!(one_2.get("title-1").cloned(), Some(40.into()));
+        let two: IndexMap<String, JsonValue> = two.get(0).unwrap().clone().try_into().unwrap();
+        assert_eq!(two.len(), 2);
+        assert_eq!(two.get("title-1").cloned(), Some(10.into()));
+        assert_eq!(two.get("title-2").cloned(), Some(150.into()));
+        Ok(())
+    }
+    #[test]
+    fn test_without_titles_return_nothing() -> Result<(), SelectionParseError> {
+        let str = ".a";
+        let grouper = Grouper::from_str(str)?;
+        struct MockOutput;
+        impl Output for MockOutput {
+            fn output_row(&mut self, _: &JsonValue, _: Vec<Option<JsonValue>>) -> std::fmt::Result {
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                None
+            }
+        }
+        let first_object = MockOutput {};
+        let started = grouper.start(
+            Arc::new(vec!["title-1".to_string(), "title-2".to_string()]),
+            Box::new(first_object),
+        );
+
+        let ret = started.without_titles();
+
+        assert_eq!(ret.is_none(), true);
+        Ok(())
+    }
+}
--- src/sorters.rs
+++ src/sorters.rs
@@ -22,7 +22,7 @@ pub enum SorterParserError {
     UnknownOrder(String),
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum Direction {
     Asc,
     Desc,
@@ -46,7 +46,9 @@ impl FromStr for Sorter {
             "" => Direction::Asc,
             "ASC" => Direction::Asc,
             "DESC" => Direction::Desc,
-            dir => return Err(SorterParserError::UnknownOrder(dir.to_string())),
+            dir => {
+                return Err(SorterParserError::UnknownOrder(dir.to_string()));
+            }
         };
         let sort_by = Arc::new(sort_by);
 
@@ -89,7 +91,7 @@ struct ActiveSorter {
 impl Output for ActiveSorter {
     fn output_row(&mut self, value: &JsonValue, row: Vec<Option<JsonValue>>) -> std::fmt::Result {
         if let Some(key) = self.sort_by.get(&Some(value.clone())) {
-            self.data.entry(key).or_default().push((value.clone(), row))
+            self.data.entry(key).or_default().push((value.clone(), row));
         }
         Ok(())
     }
@@ -114,5 +116,220 @@ impl Output for ActiveSorter {
     }
     fn without_titles(&self) -> Option<Box<dyn Output>> {
         None
+    }
+}
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+
+    use super::*;
+
+    #[test]
+    fn test_from_str_valid_name_choose_asc() -> Result<(), SorterParserError> {
+        let str = ".a";
+
+        let sorter = Sorter::from_str(str)?;
+
+        assert_eq!(sorter.direction, Direction::Asc);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_valid_name_and_asc_choose_asc() -> Result<(), SorterParserError> {
+        let str = ".a asc";
+
+        let sorter = Sorter::from_str(str)?;
+
+        assert_eq!(sorter.direction, Direction::Asc);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_valid_name_and_upercase_asc_choose_asc() -> Result<(), SorterParserError> {
+        let str = ".a aSc";
+
+        let sorter = Sorter::from_str(str)?;
+
+        assert_eq!(sorter.direction, Direction::Asc);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_valid_name_and_desc_choose_desc() -> Result<(), SorterParserError> {
+        let str = ".a DESC";
+
+        let sorter = Sorter::from_str(str)?;
+
+        assert_eq!(sorter.direction, Direction::Desc);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_valid_name_and_upercase_desc_choose_desc() -> Result<(), SorterParserError> {
+        let str = ".a DEsC";
+
+        let sorter = Sorter::from_str(str)?;
+
+        assert_eq!(sorter.direction, Direction::Desc);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_invalid_name_return_error() -> Result<(), SorterParserError> {
+        let str = "(";
+
+        let result = Sorter::from_str(str);
+
+        assert!(matches!(result, Err(_)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_invalid_sort() -> Result<(), SorterParserError> {
+        let str = ".a bla";
+
+        let result = Sorter::from_str(str);
+
+        assert!(matches!(result, Err(_)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_something_after_sort() -> Result<(), SorterParserError> {
+        let str = ".a desci";
+
+        let result = Sorter::from_str(str);
+
+        assert!(matches!(result, Err(_)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str_something_after_sort_with_space() -> Result<(), SorterParserError> {
+        let str = ".a desc .a";
+
+        let result = Sorter::from_str(str);
+
+        assert!(matches!(result, Err(_)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_asc_sort_correctly() -> Result<(), SorterParserError> {
+        let str = ". asc";
+
+        let sorter = Sorter::from_str(str)?;
+        let got_values = Arc::new(Mutex::new(Vec::new()));
+        struct MockOutput {
+            got_values: Arc<Mutex<Vec<usize>>>,
+        }
+        impl Output for MockOutput {
+            fn output_row(
+                &mut self,
+                value: &JsonValue,
+                _: Vec<Option<JsonValue>>,
+            ) -> std::fmt::Result {
+                if let Ok(num) = value.clone().try_into() {
+                    let mut got_values = self.got_values.lock().unwrap();
+                    got_values.push(num);
+                }
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                None
+            }
+        }
+
+        let mut pipe = sorter.start(Box::new(MockOutput {
+            got_values: got_values.clone(),
+        }));
+        pipe.output_row(&JsonValue::Null, vec![]).unwrap();
+        pipe.output_row(&(20).into(), vec![]).unwrap();
+        pipe.output_row(&(21).into(), vec![]).unwrap();
+        pipe.output_row(&(15).into(), vec![]).unwrap();
+        pipe.output_row(&(15).into(), vec![]).unwrap();
+        pipe.output_row(&(40).into(), vec![]).unwrap();
+        pipe.output_row(&(12).into(), vec![]).unwrap();
+        pipe.done().unwrap();
+
+        let lst = got_values.lock().unwrap();
+        assert_eq!(*lst, vec![12, 15, 15, 20, 21, 40]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_desc_sort_correctly() -> Result<(), SorterParserError> {
+        let str = ". desc";
+
+        let sorter = Sorter::from_str(str)?;
+        let got_values = Arc::new(Mutex::new(Vec::new()));
+        struct MockOutput {
+            got_values: Arc<Mutex<Vec<usize>>>,
+        }
+        impl Output for MockOutput {
+            fn output_row(
+                &mut self,
+                value: &JsonValue,
+                _: Vec<Option<JsonValue>>,
+            ) -> std::fmt::Result {
+                if let Ok(num) = value.clone().try_into() {
+                    let mut got_values = self.got_values.lock().unwrap();
+                    got_values.push(num);
+                }
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                None
+            }
+        }
+
+        let mut pipe = sorter.start(Box::new(MockOutput {
+            got_values: got_values.clone(),
+        }));
+        pipe.output_row(&JsonValue::Null, vec![]).unwrap();
+        pipe.output_row(&(20).into(), vec![]).unwrap();
+        pipe.output_row(&(21).into(), vec![]).unwrap();
+        pipe.output_row(&(15).into(), vec![]).unwrap();
+        pipe.output_row(&(15).into(), vec![]).unwrap();
+        pipe.output_row(&(40).into(), vec![]).unwrap();
+        pipe.output_row(&(12).into(), vec![]).unwrap();
+        pipe.done().unwrap();
+
+        let lst = got_values.lock().unwrap();
+        assert_eq!(*lst, vec![40, 21, 20, 15, 15, 12]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_without_titles_return_nothing() -> Result<(), SorterParserError> {
+        let str = ".a";
+        let sorter = Sorter::from_str(str)?;
+        struct MockOutput;
+        impl Output for MockOutput {
+            fn output_row(&mut self, _: &JsonValue, _: Vec<Option<JsonValue>>) -> std::fmt::Result {
+                Ok(())
+            }
+            fn without_titles(&self) -> Option<Box<dyn Output>> {
+                None
+            }
+        }
+        let first_object = MockOutput {};
+        let started = sorter.start(Box::new(first_object));
+
+        let ret = started.without_titles();
+
+        assert_eq!(ret.is_none(), true);
+        Ok(())
     }
 }
