64ca87bfcfbaa44c6b3dd1a96aad16918e076385
yift <me@ykaplan.me>
2023-12-30 15:38:09 +0000

    entries

--- src/functions/object_functions.rs
+++ src/functions/object_functions.rs
@@ -64,6 +64,37 @@ pub fn get_object_functions() -> FunctionsGroup {
         )
 
         .add_function(
+            FunctionDefinitions::new("entries", 1, 1, |args| {
+                struct Impl(Vec<Rc<dyn Get>>);
+                impl Get for Impl {
+                    fn get(&self, value: &Context) -> Option<JsonValue> {
+                        if let Some(JsonValue::Object(map)) = self.0.apply(value, 0) {
+                            let mut list = Vec::with_capacity(map.len());
+                            for (k, v) in map {
+                                let mut data = IndexMap::with_capacity(2);
+                                data.insert("value".to_string(), v.clone());
+                                data.insert("key".to_string(), k.into());
+                                list.push(data.into());
+                            }
+                            Some(JsonValue::Array(list))
+                        } else {
+                            None
+                        }
+                    }
+                }
+                Rc::new(Impl(args))
+            })
+                .add_alias("to-list")
+                .add_description_line("Get the list of all the entries of an obejct. Each item of the list will be an object with key` and `value` entries")
+                .add_example(
+                    Example::new()
+                        .add_argument(r#"{"key-1": 1, "key-2": false}"#)
+                        .expected_output(r#"[{"key": "key-1", "value": 1}, {"key": "key-2", "value": false}]"#)
+                )
+                .add_example(Example::new().add_argument("[1, 2, 4]"))
+        )
+
+        .add_function(
             FunctionDefinitions::new("sort_by_keys", 1, 1, |args| {
                 struct Impl(Vec<Rc<dyn Get>>);
                 impl Get for Impl {
