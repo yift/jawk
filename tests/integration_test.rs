3415f290f7187c05cc9dde8ce3f8b00133e732f7
yift <me@ykaplan.me>
2024-01-07 20:32:15 +0000

    Test on Windows

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -36,7 +36,26 @@ fn test_example(dir: &Path) {
     let cli = Cli::parse_from(args);
     let stdout = Rc::new(RefCell::new(Vec::new()));
     let stderr = Rc::new(RefCell::new(Vec::new()));
-    let input_file = Rc::new(dir.join("input.txt").clone());
+    let input_file = if cfg!(windows) {
+        let file = dir.join("input.txt");
+        if file.exists() {
+            let mut input_file = File::open(file).unwrap();
+            let mut input_text = String::new();
+            input_file.read_to_string(&mut input_text).unwrap();
+            input_text = input_text.replace("\r", "");
+
+            let target_dir = dir.join("target");
+            fs::create_dir_all(&target_dir).ok();
+            let tmp = target_dir.join("input.txt");
+            fs::write(&tmp, input_text).unwrap();
+            tmp
+        } else {
+            dir.join("input.txt")
+        }
+    } else {
+        dir.join("input.txt")
+    };
+    let input_file = Rc::new(input_file.clone());
     let stdin = Box::new(move || File::open(input_file.deref().clone()).unwrap());
     let master = Master::new(cli, stdout.clone(), stderr.clone(), stdin);
     let result = master.go();
@@ -64,17 +83,32 @@ fn test_example(dir: &Path) {
         let mut error_file = File::open(error_file).unwrap();
         let mut expected_error = String::new();
         error_file.read_to_string(&mut expected_error).unwrap();
+        if cfg!(windows) {
+            expected_error = expected_error.replace("\r", "");
+        }
         assert_eq!(expected_error, error_text);
     } else if !error_text.is_empty() {
         panic!("Expecting no error, got {}", error_text);
     }
 
-    let output_file = dir.join("output.txt");
+    let output_file = if cfg!(windows) {
+        let windows_file = dir.join("output.windows.txt");
+        if windows_file.exists() {
+            windows_file
+        } else {
+            dir.join("output.txt")
+        }
+    } else {
+        dir.join("output.txt")
+    };
     let output_text = String::from_utf8(stdout.borrow().clone()).unwrap();
     if output_file.exists() {
         let mut output_file = File::open(output_file).unwrap();
         let mut expected_output = String::new();
         output_file.read_to_string(&mut expected_output).unwrap();
+        if cfg!(windows) {
+            expected_output = expected_output.replace("\r", "");
+        }
         assert_eq!(expected_output, output_text)
     } else if !output_text.is_empty() {
         panic!("Expecting no output, got {}", output_text);
