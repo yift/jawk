10120eeea9287db0dd63bacdc37567e3733a1c2c
yift <me@ykaplan.me>
2024-01-13 21:15:11 +0000

    Print clear error

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use clap::Parser;
 use jawk::Cli;
-use jawk::{Master, Result};
+use jawk::Master;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-fn main() -> Result<()> {
+fn main() {
     let cli = Cli::parse();
     let stdout = Rc::new(RefCell::new(std::io::stdout()));
     let stdin = Box::new(std::io::stdin);
     let stderr = Rc::new(RefCell::new(std::io::stdout()));
 
     let master = Master::new(cli, stdout, stderr, stdin);
-    master.go()
+    if let Err(err) = master.go() {
+        eprintln!("{}", err);
+        std::process::exit(-1);
+    }
 }
